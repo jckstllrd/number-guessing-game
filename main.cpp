e542be4441f55bcc0246d40ae2a2e9c4a4bacfc0
jckstllrd <jackstallard17@gmail.com>
2026-03-16 17:16:15 +0000

    feat: add basic game loop on number of guesses

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
+#include <cstdlib>
 #include <iostream>
 
-void getResults(int secret, int guess) {}
+void getResults(int secret, int guess) { std::cout << "not quite the results"; }
 
 int numberGame() {
-  int secretNum;
+  int secretNum = rand() % 101;
+  std::cout << secretNum;
   int numOfGuesses;
   int levelDifficulty;
   std::cout << "Welcome to the Number Guessing Game!\n"
@@ -16,7 +18,30 @@ int numberGame() {
             << "\nEnter your choice: ";
   std::cin >> levelDifficulty;
   if (levelDifficulty == 1) {
+    numOfGuesses = 10;
     std::cout << "Great! You have selected the Easy difficulty level.\nLet's "
+                 "start the game!";
+    while (numOfGuesses > 0) {
+      int guess;
+      std::cout << "Enter your guess: ";
+      std::cin >> guess;
+      getResults(secretNum, guess);
+      numOfGuesses--;
+    }
+  } else if (levelDifficulty == 2) {
+    numOfGuesses = 5;
+    std::cout << "Great! You have selected the Medium difficulty level.\nLet's "
+                 "start the game!";
+    while (numOfGuesses > 0) {
+      int guess;
+      std::cout << "Enter your guess: ";
+      std::cin >> guess;
+      getResults(secretNum, guess);
+      numOfGuesses--;
+    }
+  } else if (levelDifficulty == 3) {
+    numOfGuesses = 3;
+    std::cout << "Great! You have selected the Hard difficulty level.\nLet's "
                  "start the game!";
     while (numOfGuesses > 0) {
       int guess;
